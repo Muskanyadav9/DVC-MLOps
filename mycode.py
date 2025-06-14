69786a9849f98ec9d5637a4fbf987fbe345b21fd
Muskan <ymuskan460@gmail.com>
2025-06-14 12:58:18 +0530

    first commit

---
+++ mycode.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+data = { 'Name': ['Muskan', 'Yogesh', 'Manish'],
+        'Age': ['19', '17', '23'],
+        'City': ['Prayagraj', 'Narnaul', 'Bokaro']
+}
+df= pd.DataFrame(data)
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+#file path
+file_path = os.path.join(data_dir,'Sample_data.csv')
+# dataframe to csv file
+df.to_csv(
+    file_path,index = False
+)
+print(f" csv file saved to {file_path}")
